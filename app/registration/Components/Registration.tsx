31091634f450576a51c0a7a4a081203f1e957545
Diana <diana.borro.dev@gmail.com>
2023-09-28 15:57:53 +0200

    [feat] image in form work in process

--- app/registration/Components/Registration.tsx
+++ app/registration/Components/Registration.tsx
@@ -6,30 +6,11 @@ import { useState } from 'react';
 
 export default function Registration() {
   const { register, handleSubmit } = useForm();
+  const [file, setFile] = useState<File | null>(null);
   const [registrationInfo, setRegistrationInfo] = useState(
     defaultRegistrationValues
   );
   const router = useRouter();
-
-  // const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-  //   e.preventDefault();
-  //   router.push('/account');
-
-  //   try {
-  //     const res = await fetch('/api/registration', {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //       body: JSON.stringify(registrationInfo),
-  //     });
-
-  //     setRegistrationInfo(defaultRegistrationValues);
-  //     return res.text;
-  //   } catch (err) {
-  //     console.error('Failed to fetch data', err);
-  //   }
-  // };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setRegistrationInfo((prevState) => ({
@@ -37,12 +18,34 @@ export default function Registration() {
       [e.target.name]: e.target.value,
     }));
   };
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = e.target.files?.[0];
+    if (selectedFile) {
+      setFile(selectedFile);
+    }
+  };
+
   return (
     <form
       className="make-offer-form"
       onSubmit={handleSubmit(async () => {
-        router.refresh()
+        router.refresh();
         router.push('/account');
+
+        if (file) {
+          try {
+            const response = await fetch('/api/upload', {
+              method: 'POST',
+              body: file,
+              headers: {
+                'Content-Type': 'image/jpeg',
+              },
+            });
+          } catch (error) {
+            console.error('Error:', error);
+          }
+        }
 
         try {
           const res = await fetch('/api/registration', {
@@ -75,7 +78,11 @@ export default function Registration() {
         placeholder="Contact Number"
         onChange={handleChange}
       />
-      {/* ImageUpload */}
+      <input
+        type="file"
+        {...register('businessPhoneNr')}
+        onChange={handleFileChange}
+      />
       <input
         {...register('businessAdress')}
         placeholder="Business Adress"
